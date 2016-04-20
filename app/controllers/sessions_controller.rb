9e0ac1ec8f136f31a688254b02c498bf5c8bed3a
Aljon Aniban <aljon.aniban@gmail.com>
2016-04-21 05:05:58 +0800

    Redirect to checklist path after sign in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,8 @@ class SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    checklists_path
+  end
 end
