e18f1ff60dd6867f8019e3cd3685d04c24d4c7f3
Aljon Aniban <aljon.aniban@gmail.com>
2016-04-23 00:14:09 +0800

    Set authentication keys for admin devise model.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,8 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:email]
 
   validates :email, presence: true
   validates :name, presence: true
