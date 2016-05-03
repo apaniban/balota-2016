142d3e9852f544f1fc70e354e78761aa572648e9
Aljon Aniban <aljon.aniban@gmail.com>
2016-05-04 03:04:25 +0800

    List all selected candidates in generated pdf.

--- app/services/checklist_pdf.rb
+++ app/services/checklist_pdf.rb
@@ -4,7 +4,68 @@ class ChecklistPdf < Prawn::Document
     @checklist = checklist
     @view = view
 
+    print_logo
+    print_president
+    print_vice_president
+    print_senators
+    print_partylist
+  end
+
+  def print_logo
+
+  end
+
+  def print_president
     text "President:"
-    text "#{@checklist.president.name}"
+    pad_bottom(20) {
+      if @checklist.president.present?
+        text "#{@checklist.president.name_with_ballot_number}"
+      else
+        text "Undecided"
+      end
+    }
+
+    stroke_horizontal_rule
+  end
+
+  def print_vice_president
+    move_down 20
+    text "Vice President:"
+
+    pad_bottom(20) {
+      if @checklist.vice_president.present?
+        text "#{@checklist.vice_president.name_with_ballot_number}"
+      else
+        text "Undecided"
+      end
+    }
+
+    stroke_horizontal_rule
+  end
+
+  def print_senators
+    move_down 20
+    text "Senators:"
+
+    pad_bottom(20) {
+      @checklist.senators.each do |senator|
+        text "#{senator.name_with_ballot_number}"
+      end
+    }
+
+    stroke_horizontal_rule
+  end
+
+  def print_partylist
+    move_down 20
+    text "Partylist:"
+
+    pad_bottom(20) {
+      if @checklist.party_list.present?
+        text "#{@checklist.party_list.name_with_ballot_number}"
+      else
+        text "Undecided"
+      end
+    }
   end
 end
