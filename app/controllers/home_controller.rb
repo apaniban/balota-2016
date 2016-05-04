12aa96e6ab05aa373fa09b157a4107fc4db4fed0
Aljon Aniban <aljon.aniban@gmail.com>
2016-05-04 22:40:44 +0800

    Move login page to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,27 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
   def index
     if user_signed_in?
-      redirect_to edit_checklist_path and return
+      redirect_to checklist_path and return
     end
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource_class
+    User
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
