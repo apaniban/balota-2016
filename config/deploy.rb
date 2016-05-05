3d5ed50513d4f7663536a533d4a62fc4140c76a0
Aljon Aniban <aljon.aniban@gmail.com>
2016-05-05 13:37:51 +0800

    Remove default env.

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,9 +39,6 @@ set :deploy_to, '/home/deploy/balota2016'
 
 set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-#set :rbenv_ruby, '2.2.3'
-
-set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 namespace :deploy do
   desc 'Restart application'
