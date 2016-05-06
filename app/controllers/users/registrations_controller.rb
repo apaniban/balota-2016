fcd97ff40009210cb59c55a1debb3fddc8ffcaea
Aljon Aniban <aljon.aniban@gmail.com>
2016-05-06 20:34:19 +0800

    Display edit account for normal registered users only.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :redirect_to_edit_checklist_path, only: [:edit, :update]
   protected
     def after_sign_up_path_for(resource)
       edit_checklist_path
+    end
+
+    def redirect_to_edit_checklist_path
+      redirect_to edit_checklist_path and return if current_user.present? && current_user.registered_using_omniauth?
     end
 end
